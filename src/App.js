cade4ffcd42cded1e1ef5cc01a00889bcb71082a
Timothy Mickle <timothyrmickle@gmail.com>
2018-08-06 22:22:46 -0500

    added search component and playlist finder component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import queryString from 'query-string';
+import SpotifyPlaylistSearch from './SpotifyPlaylistSearch';
+import SearchForSong from './SearchForSong';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       serverData: {},
-      filterString: ''
+      filterString: '',
+      playlists: [],
+      spotifyUserID: '',
+      spotifyName: '',
+      access_token: '',
     }
   }
   componentDidMount() {
@@ -18,11 +24,13 @@ class App extends Component {
     fetch('https://api.spotify.com/v1/me', {
       headers: {'Authorization': 'Bearer ' + accessToken}
     }).then(response => response.json())
-    .then(data => this.setState({
-      user: {
-        name: data.display_name
-      }
-    }))
+    .then(data => {
+      console.log(data, 'user data ME');
+      return this.setState({
+        spotifyUserID: data.id,
+        spotifyName: data.displayName,
+        access_token: accessToken,
+    }) })
 
     fetch('https://api.spotify.com/v1/me/playlists', {
       headers: {'Authorization': 'Bearer ' + accessToken, "Accept": "application/json","Content-Type": "application/json"
@@ -33,7 +41,9 @@ class App extends Component {
         console.log(data, 'data in playlist')
         return {
           name: item.name,
-          imageUrl: item.images[0].url, 
+          imageUrl: item.images.map((item)=>{
+            return item;
+          }), 
           songs: []
         }
     })
@@ -42,11 +52,24 @@ class App extends Component {
 
   } 
   render() {
-    console.log(this.state.playlists, 'this is the state')
+
+    // *** THIS IS PRACTIVE API MANGLING 
+    // console.log(this.state, 'this is the state')
+    // const playlists = this.state.playlists.map((item)=> {
+    //   console.log(item.imageUrl[0], 'this is an item')
+    //   return item.imageUrl.map((item) => {
+    //    console.log(item.url, 'imageURL map')
+    //    if (item.url === undefined) {} else {
+    //    return <img src={item.url}/> }
+    //  })
+    // })
+    // console.log(playlists);
+    console.log(this.state, 'this state')
     return (
     <div>
     <h1>TESTS</h1>
-    <img src={this.state.playlists}/>
+      <SpotifyPlaylistSearch access_token={this.state.access_token}/>
+      <SearchForSong access_token={this.state.access_token}/>
     </div>
     )
   }
